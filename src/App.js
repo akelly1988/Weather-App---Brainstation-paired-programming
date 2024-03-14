153042357ad0f411c7cc0c47f916af2e39ae175f
Andrew Kelly <andrewkelly1988@gmail.com>
2024-03-14 12:13:55 +0000

    Add API for weather

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
-import "./App.css";
+// import "./App.css";
+import { Route, Routes, BrowserRouter } from "react-router-dom";
+import Home from "./pages/Home/Home";
+import Destination from "./pages/Destination/Destination";
+import Header from "./Components/Header/Header";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />}></Route>
+          <Route path="/destination" element={<Destination />}></Route>
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/Components/Form/Form.js
@@ -0,0 +1,53 @@
+import React, { useState } from "react";
+import axios from "axios";
+
+const API_KEY_WEATHER = "96f23f416e2628f78d7fd10cc947516f";
+const BASE_URL_WEATHER = "https://api.openweathermap.org/data/2.5/weather";
+
+function Form() {
+  const [searchTerm, setSearchTerm] = useState("");
+  const [weather, setWeather] = useState("");
+  const handleChange = (event) => {
+    setSearchTerm(event.target.value);
+  };
+
+  const isFormvalid = () => {
+    if (!searchTerm) return false;
+    return true;
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    if (!isFormvalid()) {
+      return;
+    }
+
+    console.log(searchTerm);
+    const getDestination = async (searchTerm) => {
+      try {
+        const { data } = await axios.get(
+          `${BASE_URL_WEATHER}?q=${searchTerm}&appid=${API_KEY_WEATHER}`
+        );
+        console.log(data);
+        setWeather(data);
+      } catch (error) {
+        console.log("Error finding destination:", error);
+      }
+    };
+    getDestination(searchTerm);
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input
+        type="text"
+        placeholder="Search..."
+        value={searchTerm}
+        onChange={handleChange}
+      />
+      <button type="submit">Search</button>
+    </form>
+  );
+}
+
+export default Form;
